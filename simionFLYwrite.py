f62f6c8b98e357393eae3b4e629ccc170202afcb
raphaelphys <razibobaid@gmail.com>
2018-09-14 01:00:52 -0400

    Update simionFLYwrite.py

--- simionFLYwrite.py
+++ simionFLYwrite.py
@@ -16,13 +16,12 @@ def flywrite(filename):
 
     for i in np.arange(1, 10, 0.5):
         midText = "standard_beam {n = 180, tob = 0, mass = 17, charge = 1, \
-    ke ="+ str(i) + ", cwf = 1, color = 0, direction = cone_direction_distribution {\
-    axis = vector(1, 0, 0), half_angle = 180, fill = true}, position = vector(0, 0, 0)},"
+        ke ="+ str(i) + ", cwf = 1, color = 0, direction = cone_direction_distribution {\
+        axis = vector(1, 0, 0), half_angle = 180, fill = true}, position = vector(0, 0, 0)},"
+        
         f.write(midText)
+        
     f.write( "}")
-    
-    #Done writing here
-    
     f.close()
     
 if __init__==__main:
